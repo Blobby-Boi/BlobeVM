190aa61297bec5a7e0939ce87a26a84566dca76c
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-04-17 16:30:12 -0700

    Add files via upload

---
+++ root/installable-apps/chrome.sh
@@ -0,0 +1,7 @@
+echo "**** install chrome ****"
+apt update
+apt install -y wget
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+apt update
+apt install -y google-chrome-stable
