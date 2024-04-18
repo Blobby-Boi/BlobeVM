9ba03a4205e05135deb14e7b4bbadc9094603224
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-04-18 07:54:17 -0700

    Update steam.sh

--- root/installable-apps/steam.sh
+++ root/installable-apps/steam.sh
@@ -1,6 +1,6 @@
 echo "**** install steam ****"
-apt update
-apt install -y libatomic1 wget libc6:amd64 libc6:i386 libegl1:amd64 libegl1:i386 libgbm1:amd64 libgbm1:i386 libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1:amd64 libgl1:i386 steam-launcher zenity
+apt-get update
+apt-get install -y libatomic1 wget libc6:amd64 libc6:i386 libegl1:amd64 libegl1:i386 libgbm1:amd64 libgbm1:i386 libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1:amd64 libgl1:i386 steam-launcher zenity
 wget "https://steamcdn-a.akamaihd.net/client/installer/steam.deb" -O /tmp/steam.deb
 dpkg -i /tmp/steam.deb
 sleep 1
