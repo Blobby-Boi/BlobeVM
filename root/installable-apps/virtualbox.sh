92851203edf941aa150600eb27504fa00406bdab
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-04-18 07:52:22 -0700

    Update virtualbox.sh

--- root/installable-apps/virtualbox.sh
+++ root/installable-apps/virtualbox.sh
@@ -1,6 +1,6 @@
 echo "**** install VirtualBox ****"
 apt update
-apt install -y wget
+apt install -y wget gnupg2
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | tee /etc/apt/sources.list.d/virtualbox.list
 apt update
