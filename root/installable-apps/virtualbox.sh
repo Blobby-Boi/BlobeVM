9aa89a47e0425874498cd48496e5a8b313fcd9f9
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-04-18 08:47:46 -0700

    Update virtualbox.sh

--- root/installable-apps/virtualbox.sh
+++ root/installable-apps/virtualbox.sh
@@ -1,7 +1,6 @@
 echo "**** install VirtualBox ****"
 apt update
 apt install -y wget
-wget "https://download.virtualbox.org/virtualbox/6.1.30/virtualbox-6.1_6.1.30-147558~Debian~bullseye_amd64.deb" -O /tmp/virtualbox.deb
-dpkg -i /tmp/virtualbox.deb
-apt install -y -f
+wget https://download.virtualbox.org/virtualbox/6.1.30/virtualbox-6.1_6.1.30-148432~Debian~bullseye_amd64.deb -O /tmp/virtualbox.deb
+apt install -y /tmp/virtualbox.deb
 rm /tmp/virtualbox.deb
