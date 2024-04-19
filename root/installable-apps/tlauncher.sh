c9b6572e775fdfa3bf3b0949f945a7d3e0c80546
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-04-18 20:17:02 -0700

    minecraft

---
+++ root/installable-apps/tlauncher.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "**** install java ****"
+apt update
+apt install -y default-jre wget
+
+echo "**** install tlauncher ****"
+wget -O /tmp/tlauncher.jar https://tlauncher.org/jar
+mkdir -p /opt/tlauncher
+mv /tmp/tlauncher.jar /opt/tlauncher/tlauncher.jar
+
+cat > /usr/share/applications/tlauncher.desktop <<EOL
+[Desktop Entry]
+Name=TLauncher
+Comment=Custom Minecraft launcher
+Exec=java -jar /opt/tlauncher/tlauncher.jar
+Icon=/opt/tlauncher/tlauncher.png
+Terminal=false
+Type=Application
+Categories=Game;
+EOL
+
+wget -O /opt/tlauncher/tlauncher.png https://tlauncher.org/images/icon.png
+
+chmod +x /usr/share/applications/tlauncher.desktop
--- root/installable-apps/virtualbox.sh
+++
@@ -1,6 +0,0 @@
-echo "**** install VirtualBox ****"
-apt update
-apt install -y wget
-wget https://download.virtualbox.org/virtualbox/6.1.30/virtualbox-6.1_6.1.30-148432~Debian~bullseye_amd64.deb -O /tmp/virtualbox.deb
-apt install -y /tmp/virtualbox.deb
-rm /tmp/virtualbox.deb
