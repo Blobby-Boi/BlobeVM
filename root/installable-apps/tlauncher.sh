14285582cb3b1733182392e77ee564cb1fb4ba1e
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-04-19 17:23:45 -0700

    Update tlauncher.sh

--- root/installable-apps/tlauncher.sh
+++ root/installable-apps/tlauncher.sh
@@ -9,7 +9,7 @@ echo "**** Downloading TLauncher ****"
 wget -O /tmp/TLauncher.jar https://repo.tlauncher.org/update/lch/starter-core-1.11-v10.jar
 mkdir -p /opt/tlauncher
 mv /tmp/TLauncher.jar /opt/tlauncher/
-rm /tmp/Tlauncher.jar
+rm /tmp/TLauncher.jar
 
 cat > /usr/share/applications/tlauncher.desktop <<EOL
 [Desktop Entry]
